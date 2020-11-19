7e992a2c269f4b5c3ae6e96324001e39daafa220
STARKWANG <71419678+starkwang914@users.noreply.github.com>
2020-11-19 11:18:35 +0800

    Add files via upload

---
+++ Week 10-1.py
@@ -0,0 +1,11 @@
+movie_title="The Judge"
+release_year="2014"
+movie_time_min="141"
+movie_rating="7.4"
+trailer="https://reurl.cc/OqonA7"
+
+print("movie_title:" ,movie_title)
+print("release_year:" ,release_year)
+print("movie_time_min:" ,movie_time_min)
+print("movie_rating:" ,movie_rating)
+print("trailer:" ,trailer)
